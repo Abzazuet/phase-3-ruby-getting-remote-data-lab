d2fd4058d5e9b9f08db435aeb305e87e70ed4fb3
Abel Zazueta <abzazuet@amazon.com>
2022-08-08 07:55:49 -0700

    Assignment completed

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_accessor :URL
+    def initialize (url)
+        @URL = url
+    end
+    def get_response_body 
+        uri = URI.parse(@URL)
+        response = Net::HTTP.get_response(uri)  
+        response.body
+    end
+    def parse_json
+        conversion = JSON.parse(self.get_response_body)
+        conversion
+    end
+end
